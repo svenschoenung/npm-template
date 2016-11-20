2445e8e20e9b345446554d0745492a0b52cb5b45
Sven Schoenung <sven.schoenung@gmail.com>
2016-11-20 14:45:17 +0100

    Change setup.sh to work on both MacOS and Linux

--- setup.sh
+++ setup.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
 PROJECT=$1
+PID=$$
 
-find . -type f -exec sed -i "s/npm-template/$PROJECT/g" {} +
 rm -rf .git
+find . -type f -exec sed -i.bak$PID "s/npm-template/$PROJECT/g" {} +
+find . -type f -name "*.bak$PID" -exec rm {} +
 rm ./setup.sh
