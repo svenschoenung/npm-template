5510d0c50b86d2847dab0c67021351b5646c8f2d
Sven Schoenung <sven.schoenung@gmail.com>
2016-11-22 18:28:04 +0100

    Install devDependencies in setup.sh

--- setup.sh
+++ setup.sh
@@ -3,7 +3,9 @@
 PROJECT=$1
 PID=$$
 
+
 rm -rf .git
 find . -type f -exec sed -i.bak$PID "s/npm-template/$PROJECT/g" {} +
 find . -type f -name "*.bak$PID" -exec rm {} +
+npm install --save-dev chai codacy-coverage coveralls istanbul mocha mocha-lcov-reporter
 rm ./setup.sh
